ef805ee7e2a5a587240d8253a5f3329cfc621c46
kannan-2208 <kannanr2209@gmail.com>
2026-02-24 01:09:50 +0530

    Media viewer task

---
+++ backend/main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from database import engine, Base, SessionLocal
+import models
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+Base.metadata.create_all(bind=engine)
+
+app.mount("/media", StaticFiles(directory="media"), name="media")
+@app.get("/api/videos")
+def get_videos():
+    db = SessionLocal()
+    videos = db.query(models.Video).all()
+    db.close()
+    return videos
+@app.get("/api/images")
+def get_images():
+    db = SessionLocal()
+    images = db.query(models.Image).all()
+    db.close()
+    return images
+@app.get("/add-data")
+def add_data():
+    db = SessionLocal()
+
+    if db.query(models.Video).first():
+        db.close()
+        return {"message": "Data already added"}
+    videos = [
+        models.Video(title="Video 1", file_path="media/videos/11. frist estaate combator .mp4"),
+        models.Video(title="Video 2", file_path="media/videos/29 winworth walkthough  .mp4"),
+        models.Video(title="Video 3", file_path="media/videos/31.VBHC interior walkthrough .mp4"),
+    ]
+
+    images = [
+        models.Image(title="Image 1", file_path="media/images/Aqua suites 2BHK - Living Cam.jpg"),
+        models.Image(title="Image 2", file_path="media/images/C02_2 (1).png"),
+        models.Image(title="Image 3", file_path="media/images/c11.00180 (1).png"),
+        models.Image(title="Image 4", file_path="media/images/c12.00180 (1).png"),
+        models.Image(title="Image 5", file_path="media/images/C14-1 (1) (1) (1).png"),
+        models.Image(title="Image 6", file_path="media/images/C14-1 (1) (1).png"),
+    ]
+    db.add_all(videos + images)
+    db.commit()
+    db.close()
+    return {"message": "Data added successfully"}
---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+
+class Video(Base):
+    __tablename__ = "videos"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    file_path = Column(String)
+class Image(Base):
+    __tablename__ = "images"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    file_path = Column(String)
